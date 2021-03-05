b35521bbb8391d7b543aac35df432e5aa0c4b7ba
Mehedi Hassan <mehedii.hassan01@gmail.com>
2021-03-05 07:22:28 -0800

    Python Module

---
+++ area.py
@@ -0,0 +1,6 @@
+def traingle_area(b, h):
+    print(f"Area of Traingle is : {0.5 * b * h}")
+
+
+def rectangle_area(a, b):
+    print(f"Area of Rectangle is : {a * b}")
---
+++ testModule.py
@@ -0,0 +1,4 @@
+from area import rectangle_area, traingle_area
+
+traingle_area(10, 20)
+rectangle_area(10, 20)
